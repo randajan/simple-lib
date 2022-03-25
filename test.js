7a7b4a4520a4f4343325359b94ce13cef9756aa3
Jan Randa <jnranda@gmail.com>
2022-03-25 01:24:36 +0100

    update readme

--- test.js
+++ test.js
@@ -8,7 +8,7 @@ slib({
     srcdir:"test/src",                                          //direrctory of source code
     distdir:"test/dist",                                        //directory of build
     demodir:"test/demo",                                        //directory of demo
-    fetchVars:(async _=>await fs.readJSON("package.json")),     //function returning pairs of variables which were injected to demo/build/index.html
+    fetchVars:(async _=>await fs.readJSON("package.json")),     //function returning pairs which will be injected into the demo/build/index.html
     onRuntimeError:console.log,                                 //function that handle dev server runtime errors
     external:[]   
 })
